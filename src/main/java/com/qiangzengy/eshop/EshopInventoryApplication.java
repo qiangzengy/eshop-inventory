7e02658e730f945158499ce4cd93491eed0a5c2b
qiangzengy <qiangzengy@163.com>
2020-04-29 20:51:13 +0800

    xml

--- src/main/java/com/qiangzengy/eshop/EshopInventoryApplication.java
+++ src/main/java/com/qiangzengy/eshop/EshopInventoryApplication.java
@@ -1,5 +1,6 @@
 package com.qiangzengy.eshop;
 
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @MapperScan("com.qiangzeny.eshop.mapper")
 public class EshopInventoryApplication {
+
+    /*@Bean
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSource dataSource() {
+        return new DataSource();
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        //sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mybatis/*.xml"));
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mybatis/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(EshopInventoryApplication.class, args);
--- src/main/java/com/qiangzengy/eshop/entity/ProductInventory.java
+++ src/main/java/com/qiangzengy/eshop/entity/ProductInventory.java
@@ -4,6 +4,10 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
 
 /**
  * @author qiangzeng
@@ -11,7 +15,11 @@ import lombok.Data;
  */
 
 @Data
-public class ProductInventory {
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class ProductInventory implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @ApiModelProperty(value = "ID")
     @TableId(value = "id", type = IdType.ID_WORKER)
